3f5fe63661f3d58fde6f6eaa13625c83de919f56
unknown <Paul_Moura@US0008433.amr.global.sgs.com>
2015-02-10 17:00:35 -0600

    Add Stream Extension class and new property to System Properties class.

---
+++ StreamExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Custom
+{
+    public static class StreamExtensions
+    {
+        public static void SaveToFile(this Stream stream, string file, FileMode mode = FileMode.Create, FileAccess access = FileAccess.Write, FileShare share = FileShare.Read)
+        {
+            var fileStream = new FileStream(file, mode, access, share);
+            stream.CopyTo(fileStream);
+            fileStream.Close();
+            stream.Close();
+            //var buffer = new byte[1024*8];
+            //int len;
+            //while ((len = stream.Read(buffer, 0, buffer.Length)) > 0)
+            //{
+            //    fileStream.Write(buffer, 0, len);
+            //}
+        }
+    }
+}
---
+++ SystemProperties.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace Custom
+{
+    public static class SystemProperties
+    {
+        public static string CommonApplicationData
+        {
+            get
+            {
+                var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+                    Application.ProductName);
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                return path;
+            }
+        }
+
+        public static string MAC_Address
+        {
+            get
+            {
+                return (from nic in NetworkInterface.GetAllNetworkInterfaces()
+                        where nic.OperationalStatus == OperationalStatus.Up
+                        select nic.GetPhysicalAddress().ToString()).FirstOrDefault();
+            }
+        }
+    }
+}
