9efc72eca53d55cd974567ae955ff27a0480be2f
unknown <Paul_Moura@US0008433.amr.global.sgs.com>
2015-02-10 09:13:07 -0600

    General Save

--- SKeyValuePair.cs
+++ SKeyValuePair.cs
@@ -5,9 +5,11 @@ namespace Custom
     [Serializable]
     public class SKeyValuePair<T, TU>
     {
-        public T Key { get; private set; }
+        public T Key { get; set; }
 
         public TU Value { get; set; }
+
+        protected SKeyValuePair(){} 
 
         public SKeyValuePair(T key, TU value)
         {
--- Version.cs
+++ Version.cs
@@ -16,13 +16,43 @@ namespace Custom
     {
         #region VARIABLES
 
-        private int[] _revisionNumbers = new int[0];
+        private int[] RevisionNumbers
+        {
+            get
+            {
+                var sub = Value.Split('.');
+
+                var revisionNumbers = new int[sub.Length];
+
+                if (sub.Where((t, i) => !int.TryParse(t, out revisionNumbers[i])).Any())
+                {
+                    throw new Exception("The string provided did not meet the requirements.");
+                }
+
+                return revisionNumbers;
+            }
+        }
+
+        private string _value;
 
         #endregion
 
         #region PROPERTIES
 
         public bool IsRollback { get; set; }
+
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (!IsValid(value)) throw new Exception("The string provided did not meet the requirements.");
+                _value = value;
+            }
+        }
 
         #endregion
 
@@ -35,31 +65,17 @@ namespace Custom
 
         public static implicit operator Version(string str)
         {
-            var sub = str.Split('.');
-
-            var revisionNumbers = new int[sub.Length];
-
-            if (sub.Where((t, i) => !int.TryParse(t, out revisionNumbers[i])).Any())
-            {
-                throw new Exception("The string provided did not meet the requirements.");
-            }
-
-            var version = new Version
-            {
-                _revisionNumbers = revisionNumbers
-            };
-
-            return version;
+            return new Version { Value = str };
         }
 
         public static bool operator >(Version v1, Version v2)
         {
-            int len = v1._revisionNumbers.Length > v2._revisionNumbers.Length ? v1._revisionNumbers.Length : v2._revisionNumbers.Length;
+            int len = v1.RevisionNumbers.Length > v2.RevisionNumbers.Length ? v1.RevisionNumbers.Length : v2.RevisionNumbers.Length;
 
             for (int i = 0; i < len; i++)
             {
-                int n1 = v1._revisionNumbers.Length > i ? v1._revisionNumbers[i] : 0;
-                int n2 = v2._revisionNumbers.Length > i ? v2._revisionNumbers[i] : 0;
+                int n1 = v1.RevisionNumbers.Length > i ? v1.RevisionNumbers[i] : 0;
+                int n2 = v2.RevisionNumbers.Length > i ? v2.RevisionNumbers[i] : 0;
 
                 if (n1 > n2) return true;
             }
@@ -69,12 +85,12 @@ namespace Custom
 
         public static bool operator <(Version v1, Version v2)
         {
-            int len = v1._revisionNumbers.Length > v2._revisionNumbers.Length ? v1._revisionNumbers.Length : v2._revisionNumbers.Length;
+            int len = v1.RevisionNumbers.Length > v2.RevisionNumbers.Length ? v1.RevisionNumbers.Length : v2.RevisionNumbers.Length;
 
             for (int i = 0; i < len; i++)
             {
-                int n1 = v1._revisionNumbers.Length > i ? v1._revisionNumbers[i] : 0;
-                int n2 = v2._revisionNumbers.Length > i ? v2._revisionNumbers[i] : 0;
+                int n1 = v1.RevisionNumbers.Length > i ? v1.RevisionNumbers[i] : 0;
+                int n2 = v2.RevisionNumbers.Length > i ? v2.RevisionNumbers[i] : 0;
 
                 if (n2 > n1) return true;
             }
@@ -88,13 +104,25 @@ namespace Custom
 
         public override string ToString()
         {
-            var str = "";
-            foreach (int i in _revisionNumbers)
+            return Value;
+        }
+
+        public static bool IsValid(string value)
+        {
+            try
             {
-                if (str != "") str += ".";
-                str += i;
+                var s = value.Split('.');
+                foreach (string sub in s)
+                {
+                    int.Parse(sub);
+                }
             }
-            return str;
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         #endregion
